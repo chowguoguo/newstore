7bbb34d47d80690382231a1f198c39abb580f05d
chowguoguo <31806948@qq.com>
2019-11-19 00:30:38 +0800

    新建core模板

---
+++ core/src/test/java/com/quechao/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quechao.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
